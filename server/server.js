4ae80438ac6e2dbd7cc6d566d9c7727adaf175d3
Justin Cho Chun Wah <beluga55.github.com>
2023-10-29 17:13:36 +0800

    Added MongoDB

--- server/server.js
+++ server/server.js
@@ -1,46 +1,79 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import OpenAI from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
 
-dotenv.config()
+// MONGO DB
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri =
+  "mongodb+srv://chowah36:76ad3b7e46@cluster0.5i2ccr0.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+dotenv.config();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
-  })
-})
+    message: "Hello from CodeX!",
+  });
+});
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.chat.completions.create ({
+    const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [{ role: "system", content: prompt }],
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 400, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
-      bot: response.choices[0].message.content
+      bot: response.choices[0].message.content,
     });
-
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
   }
-})
+});
 
-app.listen(5001, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5001, () =>
+  console.log("AI server started on http://localhost:5000")
+);
