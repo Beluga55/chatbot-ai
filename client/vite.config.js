6eb82a64b893b3149f828774ba9f6d6c4f97bf0c
Justin Cho Chun Wah <beluga.github@gmail.com>
2023-11-07 19:04:14 +0800

    Multiple Pages With Vite

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        about: resolve(root, "about.html"),
+        chatbot: resolve(root, "chatbot.html"),
+        faq: resolve(root, "faq.html"),
+        login: resolve(root, "login.html"),
+        signup: resolve(root, "signup.html"),
+      },
+    },
+  },
+});
