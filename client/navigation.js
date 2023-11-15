8f7c9839776a8efdd5aa05f2b0a1d985c3571280
Justin Cho Chun Wah <beluga55.github.com>
2023-11-15 12:13:29 +0800

    Added Multiple New Files and Codes

--- client/navigation.js
+++ client/navigation.js
@@ -39,3 +39,16 @@ if (signoutBtn) {
 if(loginBtn) {
   loginBtn.addEventListener("click", redirectToLoginPage);
 }
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
